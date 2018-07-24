8309c88bb46e77291504ba4cb77ab27b2b256957
juliette manevy <jmanevy@z2r3p3.le-101.fr>
2018-07-24 17:28:33 +0200

    last_word

--- exam/last_word/last_word.c
+++ exam/last_word/last_word.c
@@ -6,7 +6,7 @@
 /*   By: jmanevy <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/24 16:02:23 by jmanevy      #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/24 16:54:15 by jmanevy     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/24 17:26:59 by jmanevy     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -25,7 +25,7 @@ void	ft_putstr(char *str)
 
 	i = 0;
 	j = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 127)
 			j = i + 1;
@@ -42,6 +42,7 @@ int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		ft_putstr(argv[1]);
-	write(1, "\n", 1);
+	else
+		write(1, "\n", 1);
 	return (0);
 }
