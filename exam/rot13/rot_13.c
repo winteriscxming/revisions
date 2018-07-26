8e2fe4a8eaf5c780bbe2f6c5b798e99bb8200947
juliette manevy <jmanevy@z2r2p4.le-101.fr>
2018-07-26 15:49:27 +0200

    ok

---
+++ exam/rot13/rot_13.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   rot_13.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jmanevy <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/26 14:51:20 by jmanevy      #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/26 15:32:17 by jmanevy     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			ft_putchar(str[i] + 13);
+		else if ((str[i] >= 'n'  && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+			ft_putchar(str[i] - 13);
+		else
+			ft_putchar(str[i]);
+	i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	ft_putstr(argv[1]);
+	return (0);
+}
