cddb9786993896ae0f2d605611deade72dcde599
juliette manevy <jmanevy@z2r2p4.le-101.fr>
2018-07-26 15:50:50 +0200

    ok

--- exam/search_and_replace/search_and_replace.c
+++ exam/search_and_replace/search_and_replace.c
@@ -1,4 +1,3 @@
-/* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
 /*   search_and_replace.c                             .::    .:/ .      .::   */
@@ -6,7 +5,7 @@
 /*   By: jmanevy <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/24 15:47:30 by jmanevy      #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/24 15:57:40 by jmanevy     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/25 15:03:00 by jmanevy     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +17,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char *str, char src, char rep)
+void	ft_putstr(char *str, char src, char remp)
 {
 	int i;
 
@@ -26,21 +25,21 @@ void	ft_putstr(char *str, char src, char rep)
 	while (str[i])
 	{
 		if (str[i] == src)
-			str[i] = rep;
+			str[i] = remp;
 		ft_putchar(str[i]);
-		i++;
+		i++;	
 	}
 	ft_putchar('\n');
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	if (argc != 4 || argv[2][1] != '\0' || argv[3][1] != '\0')
-	{	
+	if (argc != 4 ||  argv[2][1] != '\0' ||  argv[3][1] != '\0')
+	{
 		ft_putchar('\n');
 		return (0);
 	}
 	else
 		ft_putstr(argv[1], argv[2][0], argv[3][0]);
 	return (0);
-}	
+}
