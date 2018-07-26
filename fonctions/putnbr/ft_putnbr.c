089d8334a9bb5765f03a16b73ce527ddb6814474
juliette manevy <jmanevy@z2r2p4.le-101.fr>
2018-07-26 15:56:21 +0200

    ok

---
+++ fonctions/putnbr/ft_putnbr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_putnbr.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jmanevy <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/26 15:52:00 by jmanevy      #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/26 15:55:59 by jmanevy     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int nb2;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb2 = -nb;
+	}
+	else
+		nb2 = nb;
+	if (nb2 >= 10)
+	{
+		ft_putnbr(nb2 / 10);
+		ft_putnbr(nb2 % 10);
+	}
+	else
+		ft_putchar(nb2 = '0');
+}
